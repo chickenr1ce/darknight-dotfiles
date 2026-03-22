c0b1680df2cb85b9f974e132fa39e972a4e1d695
chickenr1ce <alexizfirdaus@gmail.com>
2026-03-22 08:28:16 +0100

    update waybar, added audio dongle

--- .config/waybar/scripts/toggle_audio.sh
+++ .config/waybar/scripts/toggle_audio.sh
@@ -10,18 +10,24 @@
 # Added 'head -n 1' to ensure we only get ONE ID if grep finds multiple
 PEBBLE_ID=$(wpctl status | sed -n '/Sinks:/,/Sources:/p' | grep "Pebble V3" | sed 's/[^0-9]*\([0-9]\+\).*/\1/' | head -n 1)
 HD_ID=$(wpctl status | grep -A 15 "Sinks:" | grep "Starship/Matisse" | sed 's/[^0-9]*\([0-9]\+\).*/\1/' | head -n 1)
+DONGLE_ID=$(wpctl status | grep -A 15 "Sinks:" | grep "AB13X" | sed 's/[^0-9]*\([0-9]\+\).*/\1/' | head -n 1)
 
 # 2. Get the current active Sink ID
 CURRENT_ID=$(wpctl status | grep -A 15 "Sinks:" | grep "*" | sed 's/[^0-9]*\([0-9]\+\).*/\1/' | head -n 1)
 
 # Log the found IDs for debugging
-echo "DEBUG: Pebble ID=$PEBBLE_ID, HD ID=$HD_ID, Current=$CURRENT_ID"
+echo "DEBUG: Pebble ID=$PEBBLE_ID, HD ID=$HD_ID, DONGLE ID=$DONGLE_ID, Current=$CURRENT_ID"
 
 # 3. Toggle Logic
 if [ "$CURRENT_ID" == "$HD_ID" ]; then
-    echo "Action: Switching to Pebble ($PEBBLE_ID)"
-    wpctl set-default "$PEBBLE_ID"
+    echo "Action: Switching to Dongle ($DONGLE_ID)"
+    wpctl set-default "$DONGLE_ID"
+    notify-send "Audio Switched" "Output: AB13X Dongle"
+elif [ "$CURRENT_ID" == "$DONGLE_ID" ]; then
+    echo "Action: Switching to Pebble ($PEBBLE_ID))"
+    wpctl set-default "$PEBBLE_ID)"
     notify-send "Audio Switched" "Output: Creative Pebble V3"
+
 else
     echo "Action: Switching to HD Audio ($HD_ID)"
     wpctl set-default "$HD_ID"
